51115d3337cbd5999c861606afce608b7da8fc1a
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2025-03-21 14:47:35 +1200

    USP-34 Added save program function

--- app/Livewire/Admin/Program.php
+++ app/Livewire/Admin/Program.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\Admin;
 
+use App\Livewire\Forms\ProgramForm;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
+
+#[Layout('components.layouts.app')]
 class Program extends Component
 {
+    public ProgramForm $form;
+
+    public function save(){
+        $this->validate();
+
+        Program::create(
+            $this->form->all()
+        );
+
+        return $this->redirect('admin.program');
+    }
+
+
     public function render()
     {
         return view('livewire.admin.program');
