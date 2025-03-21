4d2f61e9e4a48dd2bf44dbb40278c33e9738ccff
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2025-03-21 14:48:40 +1200

    USP-34 Added program form view

--- resources/views/livewire/admin/program.blade.php
+++ resources/views/livewire/admin/program.blade.php
@@ -1,3 +1,49 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-</div>
+<form wire:submit="save" class="flex flex-col gap-6">
+    <flux:heading>Create Program</flux:heading>
+<flux:subheading>Enter program information below.</flux:subheading>
+    <!-- Program Code -->
+    <flux:input
+        wire:model="form.program_code"
+        :label="__('Program code')"
+        type="text"
+        required
+        autofocus
+        :placeholder="__('BXXX')"
+    />
+
+    <!-- Program Name -->
+    <flux:input
+        wire:model="form.name"
+        :label="__('Program name')"
+        type="text"
+        required
+        autofocus
+        :placeholder="__('Bachelors of XXXX')"
+    />
+
+    <!-- Description -->
+    <flux:input
+        wire:model="form.description"
+        :label="__('Program description')"
+        type="text"
+        required
+        autofocus
+        :placeholder="__('Describe the program')"
+    />
+
+    <!-- Duration -->
+    <flux:input
+        wire:model="form.duration"
+        :label="__('Duration of program in years')"
+        type="number"
+        required
+        autocomplete="email"
+        placeholder="email@example.com"
+    />
+
+    <div class="flex items-center justify-end">
+        <flux:button type="submit" variant="primary" class="w-full">
+            {{ __('Create program') }}
+        </flux:button>
+    </div>
+</form>
