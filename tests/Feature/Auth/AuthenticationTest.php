f29690f4392b7d12a614bc4c93f621c8bb33c3b1
Pranav Chand <pranavchand777@gmail.com>
2025-06-02 18:17:06 +1200

    test: removed irrelevant autogenerated tests

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -6,11 +6,7 @@ use Livewire\Livewire;
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-test('login screen can be rendered', function () {
-    $response = $this->get('/login');
 
-    $response->assertStatus(200);
-});
 
 test('users can authenticate using the login screen', function () {
     $user = User::factory()->create();
